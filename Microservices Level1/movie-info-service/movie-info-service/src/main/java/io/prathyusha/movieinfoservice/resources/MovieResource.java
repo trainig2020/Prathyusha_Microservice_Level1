cf15ecb78c15b61dd560f5231f6372db87d82b39
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-20 15:20:41 +0530

    Add files via upload

---
+++ Microservices Level1/movie-info-service/movie-info-service/src/main/java/io/prathyusha/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,23 @@
+package io.prathyusha.movieinfoservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.prathyusha.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+	 
+		return new Movie(movieId,"Test name");
+	
+	}
+
+}
